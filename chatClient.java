186163d04a29e019c92d423cde7e98fd1d579e9f
Madison Brooks <brooksma@mail.gvsu.edu>
2017-11-27 02:06:51 -0500

    Add files via upload
    Added outline of encryption and decryption for Asymmetric and Symmetric

--- chatClient.java
+++ chatClient.java
@@ -8,6 +8,12 @@ Lab Project 4  - Client
   Individual message	                  10 -- DONE
   Client list	                          5 -- DONE
  Admin functions	                      5 -- DONE
+-------------Part Two---------------------
+Randomly generate symmetric key	            5  -- DONE
+Encrypt symmetric key with RSA pub key	    10
+Decrypt symmetric key with RSA private key  10
+Properly encrypting all chat messages	    10
+Properly decrypting all chat messages  	    10
 ******************************************/
 
 import java.io.*;
@@ -17,20 +23,70 @@ import java.nio.channels.*;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.lang.Math.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.*;
+import java.security.spec.*;
+import javax.xml.bind.DatatypeConverter;
 
 
 class chatClient{
 	public String kickUserString = "123456789GOODBYE987654321";
+	//private PrivateKey privKey; only the server knows what this is 
+    private PublicKey pubKey;
+    
+    public chatClient(){
+    	//privKey=null;
+		pubKey=null;
+	}
+	//Code provided in documentation
+	public SecretKey generateAESKey(){
+		try{
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(128);
+			SecretKey secKey = keyGen.generateKey();
+			return secKey;
+		}catch(Exception e){
+			System.out.println("Key Generation Exception");
+			System.exit(1);
+			return null;
+		}
+    }
+    //Code provided in documentation
+    public byte[] RSAEncrypt(byte[] plaintext){ //needed to encrypt with the public key, the new secret key
+		try{
+			Cipher c = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+			c.init(Cipher.ENCRYPT_MODE,pubKey);
+			byte[] ciphertext=c.doFinal(plaintext);
+			return ciphertext;
+		}catch(Exception e){
+			System.out.println("RSA Encrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    }
 
-      public chatClient(){}
+    //Code provided in documentation
+    //the Symetric method for encrypting 
+    public byte[] encrypt(byte[] plaintext, SecretKey secKey, IvParameterSpec iv){
+		try{
+			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			c.init(Cipher.ENCRYPT_MODE,secKey,iv);
+			byte[] ciphertext = c.doFinal(plaintext);
+			return ciphertext;
+		}catch(Exception e){
+			System.out.println("AES Encrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    } 
       
-      //public decrypt(){}
 
       /*
         Checks if valid ip
         @return True if string is a valid ip, else false
       */
-      public boolean isValidIP(String ip){
+    public boolean isValidIP(String ip){
       	try{
       	    if(ip == null || ip.isEmpty()){
       			return false;
@@ -59,7 +115,7 @@ class chatClient{
       	}
 	  }
 	  
-	  public static String readBufferIntoString(ByteBuffer buf){
+public static String readBufferIntoString(ByteBuffer buf){
 		byte[] bytes;
 		bytes = buf.array();
 		return new String(bytes);
@@ -71,7 +127,6 @@ class chatClient{
 
       	    //get input
       	    Console cons = System.console();
-
       	    String ipStr = "127.0.0.1"; // Default ip address
 			// @TODO uncomment this section - commented for testing
 			boolean valid = false;
@@ -86,7 +141,7 @@ class chatClient{
           		}
 			  }
 
-			  int portInt=9876; //declaring portInt so the code works
+			int portInt=9876; //declaring portInt so the code works
 			  
 			System.out.println("Now using port number "+portInt); //end of checking port number
 			InetSocketAddress insa = new InetSocketAddress(ipStr, portInt);
@@ -96,77 +151,116 @@ class chatClient{
 			ChatClientRecieveThread receiveThread = client.new ChatClientRecieveThread(sc);
 			ChatClientSendThread sendThread = client.new ChatClientSendThread(sc);
 			
+			//print the starting list
 			ByteBuffer listOfConnectedClientsBuf = ByteBuffer.allocate(1000);
 			sc.read(listOfConnectedClientsBuf);
 			String listOfConnectedClientsStr = readBufferIntoString(listOfConnectedClientsBuf);
 			System.out.println("Here are the clients that are connected to the server: " + listOfConnectedClientsStr);
-			receiveThread.start();
-			sendThread.start();
+			
+			ByteBuffer publicKey = ByteBuffer.allocate(10000);
+			
+			sc.read(publicKey);
+			//TODO get the public key in the correct formate 
+			//PublicKey pubKey = ;
+			//might need to get the key back into the PublicKey format because of tyoe errors?
+			System.out.println("Client has the public key."); //TODO fix this sending on the server side
+			
+			SecretKey s = client.generateAESKey(); //making the Symetric key
+			
+			byte encryptedsecret[] = c.RSAEncrypt(s.getEncoded()); //asymetric encryption of the key
+			SecureRandom r = new SecureRandom();
+			byte ivbytes[] = new byte[16];
+			r.nextBytes(ivbytes); //making a random IV
+			IvParameterSpec iv = new IvParameterSpec(ivbytes);
+			//TODO need to encrypt the symetric key with the public key and TODO send it
+			
+			
+			//start the threads
+			sendThread.start(); //eachtime something is sent it goes through a process
+			receiveThread.start(); 
+			
 			
 			// while(true){ // Send/recieve messages loop
 			// 	// Send Message
-			// 	String destination = cons.readLine("Who do you want to message (Enter client number or all) : "); //The other clients who can be messaged have names like 0, 1, 2, 3, etc. 
+			// 	String destination = cons.readLine("Who do you want to message (Enter client number or all) : "); 
+			// The other clients who can be messaged have names like 0, 1, 2, 3, etc. 
 			// 	String message  = cons.readLine("Message: ");
 			// 	String messageToServer = destination + "|" + message; // Will split the message on "|" on server side
 			// 	ByteBuffer sendBuf = ByteBuffer.wrap(messageToServer.getBytes());
 			// 	sc.write(sendBuf); // send the buffer with the destination and the message
 			// 	}
+			
       	}catch(IOException e){
       	    System.out.println("Got an exception: " + e);
       	}
           }
 
-
-		  class ChatClientRecieveThread extends Thread{
-			SocketChannel sc;
-			ChatClientRecieveThread(SocketChannel channel){
-				sc = channel;
-			}
-			public void run(){
+	class ChatClientRecieveThread extends Thread{
+		SocketChannel sc;
+		ChatClientRecieveThread(SocketChannel channel){
+			sc = channel;
+		}
+	
+		public void run(){
 			
 			while(true){
 				try{
-					System.out.println("Go back to what you were doing...");
+					System.out.println("\t Who do you want to message (Enter client number or all):");
+					// going back to what they were doing
 					// Recieve Message
 					ByteBuffer messageFromServer = ByteBuffer.allocate(10000);
 					sc.read(messageFromServer);
 					String receivedMessage = readBufferIntoString(messageFromServer);
 					
 					if( receivedMessage.equals(kickUserString) || receivedMessage.contains(kickUserString) || Objects.equals(receivedMessage, kickUserString)){
-						System.out.println("\n YOU ARE BEING KICKED - GOODBYE");
-						System.exit(-1); // Have to exit with code -1 so that it kills the whole 
+							System.out.println("\n YOU ARE BEING KICKED - GOODBYE");
+							sc.close();
+							System.exit(-1); // Have to exit with code -1 so that it kills the whole 
 					}else{
-						System.out.println("\n Got message: " + receivedMessage );
-					}
-				} catch(Exception e){
-					System.out.println("Got error while trying to recieve message: "  + e);
-				}
-				
-			}
-			}		
-		}
-
-		class ChatClientSendThread extends Thread{
-			SocketChannel sc;
-			ChatClientSendThread(SocketChannel channel){
-				sc = channel;
-			}
-			public void run(){
-				try{
-					Console cons = System.console();
-					while(true){ // Send/recieve messages loop
-						// Send Message
-						String destination = cons.readLine("\n Who do you want to message (Enter client number or all): "); //The other clients who can be messaged have names like 0, 1, 2, 3, etc. 
-						String message  = cons.readLine("\n Message: ");
-						String messageToServer = destination + "|" + message; // Will split the message on "|" on server side
-						ByteBuffer sendBuf = ByteBuffer.wrap(messageToServer.getBytes());
-						sc.write(sendBuf); // send the buffer with the destination and the message
+							System.out.println("\n ****Got message****: " + receivedMessage );
 						}
 				} catch(Exception e){
-					System.out.println("Got error while trying to recieve message: "  + e);
-				}
+						System.out.println("Got error while trying to recieve message: "  + e);
+					}
 				
 			}
-			}
+			
+		}		
+	}
+
+	/*
+		Sending Messages
+			-Make a new random IV
+			-Symetric encrypt message with the IV
+			-Send the message to the server
+	
+	*/
+	class ChatClientSendThread extends Thread{
+		SocketChannel sc;
+		ChatClientSendThread(SocketChannel channel){
+			sc = channel;
+		}
+		public void run(){
+			try{
+				
+				Console cons = System.console();
+				
+				
+				
+				while(true){ // Send/recieve messages loop
+							// Send Message
+					String destination = cons.readLine("\n Who do you want to message (Enter client number or all): "); 
+					//The other clients who can be messaged have names like 0, 1, 2, 3, etc. 
+					String message  = cons.readLine("\n Message: ");
+					String messageToServer = destination + "|" + message; // Will split the message on "|" on server side
+					ByteBuffer sendBuf = ByteBuffer.wrap(messageToServer.getBytes());
+					sc.write(sendBuf); // send the buffer with the destination and the message
+				}
+			} catch(Exception e){
+					System.out.println("Got error while trying to recieve message: "  + e);
+					}
+				
+		}
+	}
 
 }
--- chatServer.java
+++ chatServer.java
@@ -8,6 +8,19 @@ Lab Project 4 - Server
   Individual message	                  10 -- DONE
   Client list	                          5 -- DONE
  Admin functions	                      5 -- DONE
+-------------Part Two---------------------
+Randomly generate symmetric key	            5
+Encrypt symmetric key with RSA pub key	    10
+Decrypt symmetric key with RSA private key  10
+Properly encrypting all chat messages	    10
+Properly decrypting all chat messages  	    10
+
+
+
+Exception in thread "Thread-0" java.lang.ArrayIndexOutOfBoundsException: 1
+	at chatServer.getDestination(chatServer.java:58)
+	at chatServer$ChatServerThread.run(chatServer.java:158)
+
 ******************************************/  
 
 import java.io.*;
@@ -19,10 +32,122 @@ import java.io.File;
 import java.io.DataOutputStream;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Set;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.*;
+import java.security.spec.*;
+import javax.xml.bind.DatatypeConverter;
 
 
 
 class chatServer{
+    private PrivateKey privKey;
+    private PublicKey pubKey; //server sends the public key to all users
+    
+    //Code provided in documentation
+	public chatServer(){
+		privKey=null;
+		pubKey=null;
+	}
+	//Code provided in documentation
+	public void setPrivateKey(String filename){
+		try{
+			File f = new File(filename);
+			FileInputStream fs = new FileInputStream(f);
+			byte[] keybytes = new byte[(int)f.length()];
+			fs.read(keybytes);
+			fs.close();
+			PKCS8EncodedKeySpec keyspec = new PKCS8EncodedKeySpec(keybytes);
+			KeyFactory rsafactory = KeyFactory.getInstance("RSA");
+			privKey = rsafactory.generatePrivate(keyspec);
+		}catch(Exception e){
+			System.out.println("Private Key Exception");
+			e.printStackTrace(System.out);
+			System.exit(1);
+		}
+    }
+    //Code provided in documentation
+    public void setPublicKey(String filename){
+		try{
+			File f = new File(filename);
+			FileInputStream fs = new FileInputStream(f);
+			byte[] keybytes = new byte[(int)f.length()];
+			fs.read(keybytes);
+			fs.close();
+			X509EncodedKeySpec keyspec = new X509EncodedKeySpec(keybytes);
+			KeyFactory rsafactory = KeyFactory.getInstance("RSA");
+			pubKey = rsafactory.generatePublic(keyspec);
+		}catch(Exception e){
+			System.out.println("Public Key Exception");
+			System.exit(1);
+		}
+    }
+	//Code provided in documentation
+	public SecretKey generateAESKey(){
+		try{
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(128);
+			SecretKey secKey = keyGen.generateKey();
+			return secKey;
+		}catch(Exception e){
+			System.out.println("Key Generation Exception");
+			System.exit(1);
+			return null;
+		}
+    }
+    //Code provided in documentation
+    public byte[] RSAEncrypt(byte[] plaintext){
+		try{
+			Cipher c = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+			c.init(Cipher.ENCRYPT_MODE,pubKey);
+			byte[] ciphertext=c.doFinal(plaintext);
+			return ciphertext;
+		}catch(Exception e){
+			System.out.println("RSA Encrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    }
+    //Code provided in documentation
+    public byte[] RSADecrypt(byte[] ciphertext){
+		try{
+			Cipher c = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+			c.init(Cipher.DECRYPT_MODE,privKey);
+			byte[] plaintext=c.doFinal(ciphertext);
+			return plaintext;
+		}catch(Exception e){
+			System.out.println("RSA Decrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    }
+    //Code provided in documentation
+    public byte[] decrypt(byte[] ciphertext, SecretKey secKey, IvParameterSpec iv){
+		try{
+			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			c.init(Cipher.DECRYPT_MODE,secKey,iv);
+			byte[] plaintext = c.doFinal(ciphertext);
+			return plaintext;
+		}catch(Exception e){
+			System.out.println("AES Decrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    }
+    //Code provided in documentation
+    public byte[] encrypt(byte[] plaintext, SecretKey secKey, IvParameterSpec iv){
+		try{
+			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			c.init(Cipher.ENCRYPT_MODE,secKey,iv);
+			byte[] ciphertext = c.doFinal(plaintext);
+			return ciphertext;
+		}catch(Exception e){
+			System.out.println("AES Encrypt Exception");
+			System.exit(1);
+			return null;
+		}
+    } 
+    
 	public static ConcurrentHashMap<Integer, SocketChannel> clientMap = new ConcurrentHashMap<Integer, SocketChannel>();
 
 	public static SocketChannel getFromClientMap(int clientNum){ //Gets the socketChannel from the map
@@ -32,8 +157,6 @@ class chatServer{
 	public void putIntoClientMap(int clientNum, SocketChannel sc){ // put key-value pair into map
 		clientMap.put(clientNum, sc);
 	}
-	
-	public chatServer(){}
 
 	public static String readBufferIntoString(ByteBuffer buf){
 		byte[] bytes;
@@ -93,9 +216,38 @@ class chatServer{
 		System.out.println("These are the connected clients' names: " + setOfClients.toString());
 		return ByteBuffer.wrap(setOfClients.toString().getBytes());
 	}
+	
+	public String getStringOfClients(){
+		Set<Integer> setOfClients = clientMap.keySet();
+		return setOfClients.toString();	
+	}
 
     public static void main(String args[]){
 		chatServer server = new chatServer();
+		
+		server.setPrivateKey("RSApriv.der"); //making the private key using RSA
+		server.setPublicKey("RSApub.der");   //making the public key based on the private key
+		
+		SecretKey s = server.generateAESKey();
+		byte encryptedsecret[] = server.RSAEncrypt(s.getEncoded());
+		SecureRandom r = new SecureRandom();
+		byte ivbytes[] = new byte[16];
+		r.nextBytes(ivbytes);
+		IvParameterSpec iv = new IvParameterSpec(ivbytes);
+		
+		String plaintext = "This is a test string to encrypt";
+		byte ciphertext[] = server.encrypt(plaintext.getBytes(),s,iv);
+		
+		System.out.printf("CipherText: %s%n",DatatypeConverter.printHexBinary(ciphertext));
+		byte decryptedsecret[] = server.RSADecrypt(encryptedsecret);
+		
+		SecretKey ds = new SecretKeySpec(decryptedsecret,"AES");
+		byte decryptedplaintext[] = server.decrypt(ciphertext,ds,iv);
+		
+		String dpt = new String(decryptedplaintext);
+		System.out.printf("PlainText: %s%n",dpt);
+		
+		
 		try{
 				Boolean portNotValid = false;
 		        Console cons = System.console();
@@ -107,7 +259,7 @@ class chatServer{
 		        while(true){
 			   		SocketChannel sc = c.accept(); // get new channel for each new client that connects to our server
 					server.putIntoClientMap(clientNum, sc); //number of the client mapped with the socket channel.
-					System.out.println("Put client " + clientNum + " into map " + clientMap.toString());
+					System.out.println("Put client " + clientNum + " into map " + clientMap.toString()); //the toString does not work?
 					ChatServerThread t = server.new ChatServerThread(sc);
 					//TODO: a list of threads - soccet channels to keep track of clients
 					//TODO: print a list of available clients
@@ -135,28 +287,48 @@ class ChatServerThread extends Thread{
 			ByteBuffer listOfConnectedClients = getListOfConnectedClients();
 			sourceSocketChannel.write( listOfConnectedClients );
 			System.out.println("Sent client list");
+			
+			byte [] pkey = pubKey.getEncoded();
+			//TODO how to turn the public key into a byte buffer??
+			ByteBuffer privateKeyy = pubKey.getBytes();
+			sourceSocketChannel.write(privateKeyy);
+			System.out.println("Sent public key");
+			
+			ByteBuffer encryptedKey = ByteBuffer.allocate(10000);
+			//TODO decrypt this buffer with the private key to get the secret/symetric key
+			
+			System.out.println("___________________");
+			while(true){
+				// Read message from client
+				ByteBuffer buffer = ByteBuffer.allocate(10000);
+				sourceSocketChannel.read(buffer);
+				String msgFromClient = readBufferIntoString(buffer);
+				System.out.println("Got message from client: " + msgFromClient);
 
-			// Read message from client
-			ByteBuffer buffer = ByteBuffer.allocate(10000);
-			sourceSocketChannel.read(buffer);
-			String msgFromClient = readBufferIntoString(buffer);
-			System.out.println("Got message from client: " + msgFromClient);
-
-			// Get the destination from the message
-			int destinationInt = getDestination(msgFromClient);
-			if(destinationInt != -1){ // -1 means that there was not a specific destination
-				System.out.println("Successfully got the destination from the message - sending to: " + destinationInt);
-
-				// Instantiate the connection to the destination - use clientMap to do this
-				SocketChannel destinationSocketChannel = getFromClientMap(destinationInt);
+				// Get the destination from the message
+				int destinationInt = getDestination(msgFromClient);
+				String clientmap = getStringOfClients();
+				if(destinationInt != -1){ // -1 means that there was not a specific destination
+					String destinationS = String.valueOf(destinationInt);
+					if(!clientmap.contains(destinationS)){ //this thread is no longer in the client list 
+						System.exit(0); //so it should not be a thread anymore..
+					}
 				
-				// Get the actual message to be sent to the destination
-				ByteBuffer msgToSend = getMessage(msgFromClient);
-				System.out.println("Sending message to client: "  + msgToSend);
+					System.out.println("Successfully got the destination from the message - sending to: " + destinationInt);
+					//TODO decrypt the message with the symetic key given by the client
+					
+					// Instantiate the connection to the destination - use clientMap to do this
+					SocketChannel destinationSocketChannel = getFromClientMap(destinationInt);
+				
+					// Get the actual message to be sent to the destination
+					ByteBuffer msgToSend = getMessage(msgFromClient);
+					System.out.println("Sending message to client: "  + msgToSend);
 
-				// Send the message to the destination
-				destinationSocketChannel.write(msgToSend);
+					// Send the message to the destination
+					destinationSocketChannel.write(msgToSend);
+					System.out.println("**************************************\n");
 			}
+		}
 		
 	    }catch(IOException e){
 			System.out.println("Got an Exception: " + e);
